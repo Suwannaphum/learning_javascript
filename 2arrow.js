03bad617f1769d2497c2166d51859d209b2a48c5
Mhai <suwannaphum.ins@gmail.com>
2025-11-22 20:32:27 +0700

    I learn about arrow function EP2

---
+++ 2arrow.js
@@ -0,0 +1,11 @@
+// fullname = (fname, lname) => fname + lname;
+
+// แบบเดิม
+function fullname(fname, lname){
+    return fname + lname;
+}
+console.log(fullname("Mhai", "lele"));
+
+// แบบ Arrow Function
+me = (age, weight) => age + weight;
+console.log(me("24", " 90"));
